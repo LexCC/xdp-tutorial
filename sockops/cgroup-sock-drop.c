adfd90bcf247fff64c8cb81d3c4ed040296e5a53
swiftstack <swiftstack@s8k-sjc11-c03-mgmt-16.nsv.sjc11.nvmetal.net>
2023-02-10 13:30:54 +0000

    Ignore node internal request in socket process

--- sockops/cgroup-sock-drop.c
+++ sockops/cgroup-sock-drop.c
@@ -125,6 +125,10 @@ int cgroup_socket_drop(struct __sk_buff *skb)
 	if(ip) {
 		if (data + sizeof(struct iphdr) > data_end) { return 0; }
 		 struct iphdr *ip = data;
+		 __u32 local_IP = bpf_htonl(0b01111111000000000000000000000001); // 127.0.0.1
+		 if(ip->daddr == local_IP) {
+			return SK_PASS;
+		 }
 		 
 		 __u8 tcp = ip->protocol == IPPROTO_TCP;
 		 if(tcp) {
