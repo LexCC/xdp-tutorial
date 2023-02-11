19306b7cd4ab10b1bd9cf85006acb8e661a4221e
swiftstack <swiftstack@s8k-sjc11-c03-mgmt-16.nsv.sjc11.nvmetal.net>
2023-02-11 14:13:41 +0000

    make compiling BTF and throttling syn packet configurable

--- sockops/bpf_sockops.h
+++ sockops/bpf_sockops.h
@@ -36,12 +36,16 @@
 #define NANOSEC_PER_SEC 1000000000
 #endif
 
+#ifndef ENABLE_THROTTLE_SYN
+#define ENABLE_THROTTLE_SYN 0 // 0 disable, 1 enable
+#endif
+
 #ifndef SWIFT_PROXY_SERVER_PORT
-#define SWIFT_PROXY_SERVER_PORT 8080
+#define SWIFT_PROXY_SERVER_PORT 80
 #endif
 
 #ifndef MAX_CONN
-#define MAX_CONN 40
+#define MAX_CONN 480
 #endif
 
 #ifndef SOCKET_TIMEOUT_SEC
@@ -70,7 +74,7 @@
 #endif
 
 #ifndef printk
-# define printk(fmt, ...)                                      \
+#define printk(fmt, ...)                                      \
     ({                                                         \
         char ____fmt[] = fmt;                                  \
         trace_printk(____fmt, sizeof(____fmt), ##__VA_ARGS__); \
@@ -103,6 +107,38 @@ struct reservation {
 	__u32 syn_ack_retry;
 };
 
+#ifndef COMPILE_BTF
+#define COMPILE_BTF 1
+#endif
+
+#if COMPILE_BTF == 1
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, sizeof(__u32));
+	__type(value, sizeof(struct connection));
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+} existed_counter_map SEC(".maps");
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, 1);
+	__type(key, sizeof(__u32));
+	__type(value, sizeof(struct burst_per_open));
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+} burst_connection_map SEC(".maps");
+
+
+struct {
+	__uint(type, BPF_MAP_TYPE_HASH);
+	__uint(max_entries, MAX_CONN);
+	__type(key, sizeof(struct flow_key));
+	__type(value, sizeof(struct reservation));
+	__uint(pinning, LIBBPF_PIN_BY_NAME);
+} reservation_ops_map SEC(".maps");
+
+#else
+
 struct bpf_map_def SEC("maps") existed_counter_map = {
 	.type        = BPF_MAP_TYPE_HASH,
 	.key_size    = sizeof(__u32),
@@ -117,7 +153,6 @@ struct bpf_map_def SEC("maps") burst_connection_map = {
 	.max_entries = 1,
 };
 
-
 struct bpf_map_def __section("maps") reservation_ops_map = {
 	.type           = BPF_MAP_TYPE_HASH,
 	.key_size       = sizeof(struct flow_key),
@@ -125,6 +160,7 @@ struct bpf_map_def __section("maps") reservation_ops_map = {
 	.max_entries    = MAX_CONN,
 	.map_flags      = 0,
 };
+#endif
 
 static __always_inline
 __u32 getBootTimeSec() {
